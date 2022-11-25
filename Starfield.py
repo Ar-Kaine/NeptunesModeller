68ceb7c3b4ba78c323220a5cfe184b7b0fff3b75
JamieMacIver <jamie.maciver@gmail.com>
2022-11-25 21:07:04 +0000

    Update Starfield.py

--- Starfield.py
+++ Starfield.py
@@ -16,3 +16,28 @@ import json
 if __name__ == "__main__":
      config = json.load(open(config_file))
      connection = Connection(config['game_id'], config['api_key'])
+     
+     results = []
+     
+     for i in connection.stars.values():
+         star = {}
+         star['uid'] = int(i['uid'])
+         star['name'] = i['n']
+         star['x'] = float(i['x'])
+         star['y'] = float(i['y'])
+         star['puid'] = int(i['puid'])
+         
+         #Visible only - and try/except
+         star['r'] = int(i['r'])
+         star['ga'] = int(i['ga'])
+         star['e'] = int(i['ga'])
+         star['i'] = int(i['ga'])
+         star['r'] = int(i['ga'])                
+         star['st'] = int(i['st'])
+                             
+         #To add: Wormholes
+         
+         results.append(star)
+         
+     output = {"stars" : results}
+     json.dump(output, open(config["output_location"],'w'), indent=4)
