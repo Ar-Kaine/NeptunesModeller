a4af8631b3a9a2e503d4cf39f0dc8d2e09861143
JamieMacIver <jamie.maciver@gmail.com>
2023-06-02 17:03:27 +0100

    statistics added

--- statshack.py
+++ statshack.py
@@ -8,56 +8,71 @@ Created on Sat Dec 17 13:45:14 2022
 from scipy.stats import fisher_exact as fe
 from pandas import DataFrame as df
 
-data = { ('Off Center', 'AFK') : [[43 , 113],[200 , 584]]  , 
-        ('Off Corner', 'AFK') : [[107 , 309],[136 , 388]]  , 
-        ('True Center', 'AFK') : [[54 , 154],[189 , 543]]  , 
-        ('True Corner', 'AFK') : [[39 , 121],[204 , 576]]  , 
-        ('Off Center', 'Survived') : [[23 , 133],[182 , 602]]  , 
-        ('Off Corner', 'Survived') : [[80 , 336],[125 , 399]]  , 
-        ('True Center', 'Survived') : [[56 , 152],[149 , 583]]  , 
-        ('True Corner', 'Survived') : [[46 , 114],[159 , 621]]  , 
-        ('Off Center', 'Podium') : [[12 , 144],[109 , 675]]  , 
-        ('Off Corner', 'Podium') : [[55 , 361],[66 , 458]]  , 
-        ('True Center', 'Podium') : [[41 , 167],[80 , 652]]  , 
-        ('True Corner', 'Podium') : [[13 , 147],[108 , 672]]  ,
-        ('Off Center', 'Over 10') : [[3 , 153],[66 , 718]]  , 
-        ('Off Corner', 'Over 10') : [[19 , 397],[50 , 474]]  ,
-        ('True Center', 'Over 10') : [[15 , 193],[54 , 678]]  , 
-        ('True Corner', 'Over 10') : [[32 , 128],[37 , 743]]  ,
-        ('Off Center', 'Winner') : [[1 , 155],[17 , 767]]  , 
-        ('Off Corner', 'Winner') : [[6 , 410],[12 , 512]]  , 
-        ('True Center', 'Winner') : [[6 , 202],[12 , 720]]  ,
-        ('True Corner', 'Winner') : [[5 , 155],[13 , 767]]  } 
+data = { ('Outside Center', 'AFK') : [[142 , 402],[449 , 1183]]  , 
+        ('Off Corner', 'AFK') : [[231 , 585],[360 , 1000]]  , 
+        ('Inside Center', 'AFK') : [[78 , 194],[513 , 1391]]  , 
+        ('True Corner', 'AFK') : [[140 , 404],[451 , 1181]]  , 
+        ('Outside Center', 'Survived') : [[86 , 458],[329 , 1303]]  , 
+        ('Off Corner', 'Survived') : [[161 , 655],[254 , 1106]]  , 
+        ('Inside Center', 'Survived') : [[34 , 238],[381 , 1523]]  , 
+        ('True Corner', 'Survived') : [[134 , 410],[281 , 1351]]  , 
+        ('Outside Center', 'Podium') : [[21 , 523],[81 , 1551]]  , 
+        ('Off Corner', 'Podium') : [[33 , 783],[69 , 1291]]  , 
+        ('Inside Center', 'Podium') : [[9 , 263],[93 , 1811]]  , 
+        ('True Corner', 'Podium') : [[39 , 505],[63 , 1569]]  , 
+        ('Outside Center', 'Over 10') : [[50 , 494],[241 , 1391]]  , 
+        ('Off Corner', 'Over 10') : [[113 , 703],[178 , 1182]]  , 
+        ('Inside Center', 'Over 10') : [[19 , 253],[272 , 1632]]  , 
+        ('True Corner', 'Over 10') : [[109 , 435],[182 , 1450]]  , 
+        ('Outside Center', 'Winner') : [[4 , 540],[30 , 1602]]  , 
+        ('Off Corner', 'Winner') : [[13 , 803],[21 , 1339]]  , 
+        ('Inside Center', 'Winner') : [[2 , 270],[32 , 1872]]  , 
+        ('True Corner', 'Winner') : [[15 , 529],[19 , 1613]]  } 
 
 
+data2 = { ('Outside Center', 'AFK') : [[142 , 402],[309 , 779]]  , 
+         ('Off Corner', 'AFK') : [[231 , 585],[220 , 596]]  ,
+         ('Inside Center', 'AFK') : [[78 , 194],[373 , 987]]  , 
+         ('Outside Center', 'Survived') : [[86 , 458],[195 , 893]]  ,
+         ('Off Corner', 'Survived') : [[161 , 655],[120 , 696]]  , 
+         ('Inside Center', 'Survived') : [[34 , 238],[247 , 1113]]  , 
+         ('Outside Center', 'Podium') : [[21 , 523],[42 , 1046]]  , 
+         ('Off Corner', 'Podium') : [[33 , 783],[30 , 786]]  , 
+         ('Inside Center', 'Podium') : [[9 , 263],[54 , 1306]]  , 
+         ('Outside Center', 'Over 10') : [[50 , 494],[132 , 956]]  , 
+         ('Off Corner', 'Over 10') : [[113 , 703],[69 , 747]]  , 
+         ('Inside Center', 'Over 10') : [[19 , 253],[163 , 1197]]  , 
+         ('Outside Center', 'Winner') : [[4 , 540],[15 , 1073]]  , 
+         ('Off Corner', 'Winner') : [[13 , 803],[6 , 810]]  , 
+         ('Inside Center', 'Winner') : [[2 , 270],[17 , 1343]]  , }
 
 
 
 
 tests =  ['two-sided','less','greater']
-finalresults = {'Survived' : {'Off Center' : {} ,
+finalresults = {'Survived' : {'Outside Center' : {} ,
                               'Off Corner' : {} ,
-                              'True Center' : {} ,
+                              'Inside Center' : {} ,
                               'True Corner' : {}} ,
-                'Podium' : {'Off Center' : {} ,
+                'Podium' : {'Outside Center' : {} ,
                               'Off Corner' : {} ,
-                              'True Center' : {} ,
+                              'Inside Center' : {} ,
                               'True Corner' : {}} ,
-               'Winner' : {'Off Center' : {} ,
+               'Winner' : {'Outside Center' : {} ,
                               'Off Corner' : {} ,
-                              'True Center' : {} ,
+                              'Inside Center' : {} ,
                               'True Corner' : {}} ,
-                'Over 10' : {'Off Center' : {} ,
+                'Over 10' : {'Outside Center' : {} ,
                               'Off Corner' : {} ,
-                              'True Center' : {} ,
+                              'Inside Center' : {} ,
                               'True Corner' : {}} ,
-                'AFK' :{'Off Center' : {} ,
+                'AFK' :{'Outside Center' : {} ,
                               'Off Corner' : {} ,
-                              'True Center' : {} ,
+                              'Inside Center' : {} ,
                               'True Corner' : {}} 
                 }
 
-for k,v in data.items():
+for k,v in data2.items():
     a = k[1]
     b = k[0]
     for i in tests:
@@ -70,3 +85,37 @@ podium = df(finalresults['Podium']).transpose()
 overten = df(finalresults['Over 10']).transpose()
 afk = df(finalresults['AFK']).transpose()
 winner = df(finalresults['Winner']).transpose()
+
+
+
+
+finalresults2 = {'Survived' : {'Outside Center' : {} ,
+                              'Off Corner' : {} ,
+                              'Inside Center' : {}} ,
+                'Podium' : {'Outside Center' : {} ,
+                              'Off Corner' : {} ,
+                              'Inside Center' : {}} ,
+               'Winner' : {'Outside Center' : {} ,
+                              'Off Corner' : {} ,
+                              'Inside Center' : {}} ,
+                'Over 10' : {'Outside Center' : {} ,
+                              'Off Corner' : {} ,
+                              'Inside Center' : {}} ,
+                'AFK' :{'Outside Center' : {} ,
+                              'Off Corner' : {} ,
+                              'Inside Center' : {}} 
+                }
+
+for k,v in data2.items():
+    a = k[1]
+    b = k[0]
+    for i in tests:
+        results = fe(v,i)[1]
+        results = round(results*100,2)
+        finalresults2[a][b][i] = results
+
+survived2 = df(finalresults2['Survived']).transpose()
+podium2 = df(finalresults2['Podium']).transpose()    
+overten2 = df(finalresults2['Over 10']).transpose()
+afk2 = df(finalresults2['AFK']).transpose()
+winner2 = df(finalresults2['Winner']).transpose()
